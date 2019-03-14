a4f53aa4f5fe099bc4cf3761928632e72e0de312
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-03-14 19:21:40 -0400

    basic structure

--- src/index.js
+++ src/index.js
@@ -2,42 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Roof extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGreen: false
+    };
+  }
+
   render() {
+    const toggle = () => this.setState({isGreen: !this.state.isGreen})
+    const bgColor = this.state.isGreen ? 'green' : 'gray'
+    const w = this.props.width
+    const h = this.props.height
+
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="roof" onClick={toggle} style={{backgroundColor: bgColor, width: w, height: h}}>
+        {/* TODO add dropdown menu for options */}
       </button>
-    );
+    )
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
   render() {
-    const status = 'Next player: X';
+    var rows = []
+    const numrows = 20
+    const numcols = 20
+
+    for (var i = 0; i < numrows; i++) {
+      var cols = []
+      for (var j = 0; j < numcols; j++) {
+        cols.push(<Roof width={this.props.width / 20} height={this.props.height / 20} />)
+      }
+      rows.push(<div>{cols}</div>)
+    }
 
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="board" style={{width: this.props.width, height: this.props.height}}>
+        {rows}
       </div>
     );
   }
@@ -47,12 +50,22 @@ class Game extends React.Component {
   render() {
     return (
       <div className="game">
-        <div className="game-board">
-          <Board />
+        <div className="centered">
+          <h2 style={{color: 'white'}}>Stormwater Game {/* TODO: better title */}</h2>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+        <div className="centered">
+          <div className="light-blue game-col">
+            { /* TODO: instructions */ }
+          </div>
+          <Board width={this.props.width} height={this.props.height}/>
+          <div className="light-green game-col">
+            { /* TODO: */ }
+          </div>
+        </div>
+        <div className="padding">
+          <div className="light-gray">
+            { /* TODO: legend */ }
+          </div>
         </div>
       </div>
     );
@@ -62,6 +75,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Game width={1000} height={500}/>,
   document.getElementById('root')
 );
